bbac9cfdc5693015809158f2da3d0931ed52612a
MagicPotatoBean <121609915+MagicPotatoBean@users.noreply.github.com>
2023-11-22 10:23:19 +0000

    Move code to simplify request handling

--- src/main.rs
+++ src/main.rs
@@ -23,41 +23,10 @@ fn host_server(port: String) {
         };
     }
 }
-fn handle_connection(mut stream: TcpStream) {
-    let ip = stream.peer_addr().expect("Failed to read peer ip").to_string();
-    let buf_reader = BufReader::new(&mut stream);
-
-    let request = match buf_reader.lines().next() {
-        Some(value) => match value {
-            Ok(request) => request,
-            Err(_) => {
-                print_time(format!("{}: Encountered problem while reading request from {}", "Error".red() , ip));
-                return;
-            },
-        },
-        None => {
-            print_time(format!("{}: No request supplied by {}", "Error".red(), ip));
-            return;
-        },
-    };
-
-    print_time(format!("Info: Serving {} request \"{}\"", ip, request));
-    if        request == "GET / HTTP/1.1" {
-        send_text("src\\webpage\\main\\index.html", &mut stream)
-    } else if request == "GET /favicon.ico HTTP/1.1" {
-        send_img("src\\webpage\\main\\assets\\favicon.ico", &mut stream)
-    } else if request == "GET /mossy HTTP/1.1" {
-        send_text("src\\webpage\\mossy\\index.html", &mut stream)
-    } else if request == "GET /mossy/style.css HTTP/1.1" {
-        send_text("src\\webpage\\mossy\\assets\\style.css", &mut stream)
-    } else if request == "GET /mossy/Mossy.jpeg HTTP/1.1" {
-        send_img("src\\webpage\\mossy\\assets\\Mossy.jpg", &mut stream)
-    } else if request == "GET /mossy/Wikipedia.png HTTP/1.1" {
-        send_img("src\\webpage\\mossy\\assets\\Wikipedia.png", &mut stream)
-    } else {
-        print_time(format!("{}: Unknown request \"{}\" from {}", "Warn".yellow(), request, ip));
-        send_text("src\\webpage\\404.html", &mut stream)
-    }
+fn print_time(text: String) {
+    let since_the_epoch = Local::now()
+    .format("%Y-%m-%d][%H:%M:%S");
+println!("[{}]: {}", since_the_epoch, text);
 }
 fn send_text(path: &str, stream: &mut TcpStream) {
     let status_line = "HTTP/1.1 200 OK";
@@ -88,8 +57,36 @@ fn send_img(path: &str, stream: &mut TcpStream) {
     let _ = stream.write_all(response.as_bytes());
     let _ = stream.write_all(&contents);
 }
-fn print_time(text: String) {
-    let since_the_epoch = Local::now()
-    .format("%Y-%m-%d][%H:%M:%S");
-    println!("[{}]: {}", since_the_epoch, text);
+fn handle_connection(mut stream: TcpStream) {
+    let ip = stream.peer_addr().expect("Failed to read peer ip").to_string();
+    let buf_reader = BufReader::new(&mut stream);
+
+    let mut request_lines = buf_reader.lines();
+    let header = match request_lines.next() {
+        Some(value) => match value {
+            Ok(request) => request,
+            Err(_) => {
+                print_time(format!("{}: Encountered problem while reading request from {}", "Error".red() , ip));
+                return;
+            },
+        },
+        None => {
+            print_time(format!("{}: \"{}\" supplied no request", "Error".red(), ip));
+            return;
+        },
+    };
+    let body: Vec<_> = request_lines.map(|result| result.unwrap())
+    .take_while(|line| !line.is_empty())
+    .collect();
+
+
+    print_time(format!("Info: Serving {} request \"{}\"", ip, header));
+    if        header == "GET / HTTP/1.1" {
+        send_text("src\\webpage\\main\\index.html", &mut stream)
+    } else if header == "GET /favicon.ico HTTP/1.1" {
+        send_img("src\\webpage\\main\\assets\\favicon.ico", &mut stream)
+    } else {
+        print_time(format!("{}: Unknown request from {}: \"{}\", data: \n\"{:#?}\"", "Warn".yellow(), ip, header, body));
+        send_text("src\\webpage\\404.html", &mut stream)
+    }
 }
